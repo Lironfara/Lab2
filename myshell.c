b5d06348c3e4b6aa2b7162000cf48785ebdf0098
Lironfara <lironfarag34@gmail.com>
2024-11-26 12:14:53 +0200

    fixing exevp error

--- myshell.c
+++ myshell.c
@@ -13,33 +13,73 @@
 
 #define MAX_LENGTH 2048
 #define MAX_PATH_SIZE 4096
-
 //for reading string - array of chars:
 
 void execute(cmdLine *pCmdLine){
-    //recieves a parsed line
-    int PID = fork(); //maintian shell alive
-    if (PID == -1){
-        perror("Frok failed");
-        freeCmdLines(pCmdLine);
-        _exit(1);
-    }
 
-    else if (PID == 0){ //child process
-        if (execvp(pCmdLine->arguments[0], pCmdLine->arguments) == -1){ //trying to run
-        perror("Error in execvp");
-        freeCmdLines(pCmdLine);
-        _exit(1); //to 'kill' the child :(
-    
-    }
-  }
-    else{ //the parent
-        if (pCmdLine->blocking ==1) {
-            int status = 0;
-            waitpid(PID, &status, 0);
+        if (strcmp (pCmdLine->arguments[0], "stop") == 0){
+            int childPID = atoi(pCmdLine->arguments[1]);
+            if (kill(childPID, SIGSTOP) == -1){
+                perror("Fail to stop process");
+            }
+            else {
+                fprintf(stdout, "Process %d was stopped\n", childPID);
+            }
         }
 
-    }
+         else if (strcmp (pCmdLine->arguments[0],"wake") == 0){
+            int childPID = atoi(pCmdLine->arguments[1]);
+            if (kill(childPID, SIGCONT) == -1){
+                perror("Fail to wake process");
+            }
+            else {
+                fprintf(stdout, "Process %d was woken\n", childPID);
+            }
+        }
+
+        else if (strcmp (pCmdLine->arguments[0],"term") == 0){
+            int childPID = atoi(pCmdLine->arguments[1]);
+            if (kill(childPID, SIGTERM) == -1){
+                perror("Fail to terminate process");
+            }
+            else {
+                fprintf(stdout, "Process %d was terminated\n", childPID);
+            }
+        }
+
+        else if (strcmp (pCmdLine->arguments[0], "cd") == 0){
+            if (chdir(pCmdLine->arguments[1]) == -1){ //trying yto change directory of the shell
+                perror("Fail in chdir");
+            }
+            freeCmdLines(pCmdLine);
+        }
+
+        else{
+            int PID = fork(); //maintian shell alive
+            if (PID == -1){
+            perror("Frok failed");
+            freeCmdLines(pCmdLine);
+            _exit(1);
+            }
+
+            else if (PID == 0){ //child process
+            if (execvp(pCmdLine->arguments[0], pCmdLine->arguments) == -1){ //trying to run
+            perror("Error in execvp");
+            freeCmdLines(pCmdLine);
+            _exit(1); //to 'kill' the child :(
+            }
+        }
+            else{ //the parent
+                if (pCmdLine->blocking ==1) {
+                int status = 0;
+                waitpid(PID, &status, 0);
+                }
+
+                }
+        }
+
+    //recieves a parsed line
+    
 } 
     
 
@@ -63,7 +103,7 @@ int main(int argc, char **argv){
             _exit(1);
         }
 
-      
+    
 
         fprintf(stdout, "%s>", current_dir); //print the current directory using prompt symbol
         char input[MAX_LENGTH]; //string is array of chars
@@ -74,50 +114,14 @@ int main(int argc, char **argv){
             fprintf(stderr, "PID: %d\n", getpid());
             fprintf(stderr, "Executing command: %s", input);
         }
-        
-        parsedLine = parseCmdLines(input); //cmd structure    
-        if (strcmp (parsedLine->arguments[0], "quit") == 0 ){
-            freeCmdLines(parsedLine); //avoding memory leak
+
+
+        if (strcmp (input,  "quit\n") == 0 ){
             break;
         }
-
-        if (strcmp (parsedLine->arguments[0], "stop") == 0){
-            int childPID = atoi(parsedLine->arguments[1]);
-            if (kill(childPID, SIGSTOP) == -1){
-                perror("Fail to stop process");
-            }
-            else {
-                fprintf(stdout, "Process %d was stopped\n", childPID);
-            }
-        }
-
-         if (strcmp (parsedLine->arguments[0],"wake") == 0){
-            int childPID = atoi(parsedLine->arguments[1]);
-            if (kill(childPID, SIGCONT) == -1){
-                perror("Fail to wake process");
-            }
-            else {
-                fprintf(stdout, "Process %d was woken\n", childPID);
-            }
-        }
-
-        if (strcmp (parsedLine->arguments[0],"term") == 0){
-            int childPID = atoi(parsedLine->arguments[1]);
-            if (kill(childPID, SIGTERM) == -1){
-                perror("Fail to terminate process");
-            }
-            else {
-                fprintf(stdout, "Process %d was terminated\n", childPID);
-            }
-        }
-
-        if (strcmp (parsedLine->arguments[0], "cd") == 0){
-            if (chdir(parsedLine->arguments[1]) == -1){ //trying yto change directory of the shell
-                perror("Fail in chdir");
-            }
-            freeCmdLines(parsedLine);
-            continue;
-        }
+        
+        parsedLine = parseCmdLines(input); //cmd structure    
+        
 
         execute(parsedLine); 
         freeCmdLines(parsedLine); 
